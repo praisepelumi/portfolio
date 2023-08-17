fcbdc5a3e6d9761578e79e201662256236f23002
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-17 13:31:37 -0400

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: path.join(__dirname, './src/index.ts'),
+  output: {
+    path: path.resolve(__dirnmae, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devtool: 'eval-source-map',
+  mode: process.env.NODE_ENV,
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true,
+    historyApiFallback: true,
+
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    proxy: {
+      '/assets/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+      '/server/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      }, 
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  esolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+
+}
