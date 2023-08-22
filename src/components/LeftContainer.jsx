102012934839b2f9ef8dd66ee1bd392b54b4bd8e
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-22 15:48:10 -0400

    started setting up LeftContainer

--- src/components/LeftContainer.jsx
+++ src/components/LeftContainer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function LeftContainer () {
-  
+  return (
+    <div>
+      <h1>Praise Emmanuel</h1>
+      <h2>Software Engineer</h2>
+    </div>
+  )
 }
 
 export default LeftContainer;
