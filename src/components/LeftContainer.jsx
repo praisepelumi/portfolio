fc40441c51b1e6dafb2a40a4aac9901bc6eb60a5
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-22 15:59:49 -0400

    defined initial info

--- src/components/LeftContainer.jsx
+++ src/components/LeftContainer.jsx
@@ -5,6 +5,15 @@ function LeftContainer () {
     <div>
       <h1>Praise Emmanuel</h1>
       <h2>Software Engineer</h2>
+
+      <div>
+        <p>
+          I am Software Engineer who is passionate about solving complex problems and building robust applications. 
+        </p>
+        <p>
+          When I'm not coding, you can find me cooking, interior decorating, playing the piano, or playing the guitar.
+        </p>
+      </div>
     </div>
   )
 }
