5c6ee02d868edfca06539c72208b85ddcf4ccfbd
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-22 16:20:26 -0400

    Started HTML structure of Left container

--- src/components/LeftContainer.jsx
+++ src/components/LeftContainer.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 function LeftContainer () {
   return (
     <div>
+      <div>
       <h1>Praise Emmanuel</h1>
       <h2>Software Engineer</h2>
+      </div>
 
       <div>
         <p>
@@ -13,6 +15,22 @@ function LeftContainer () {
         <p>
           When I'm not coding, you can find me cooking, interior decorating, playing the piano, or playing the guitar.
         </p>
+      </div>
+
+      <nav>
+        <ul>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Resume</li>
+        </ul>
+      </nav>
+
+      <div>
+        <ul>
+          <li>Github</li>
+          <li>Linkedin</li>
+          <li>Email Me</li>
+        </ul>
       </div>
     </div>
   )
