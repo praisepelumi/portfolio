9181e7fa20815f3764b2d75756f2c043d415b1d5
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-22 17:14:33 -0400

    Started with Right Container

--- src/components/RightContainer.jsx
+++ src/components/RightContainer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function RightContainer () {
-
+  
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
 };
 
 export default RightContainer;
