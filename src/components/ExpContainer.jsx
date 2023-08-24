aaff178d85502868efb94c292a7a6c52573a3127
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-24 13:07:34 -0400

    defined ExpContainer imports

--- src/components/ExpContainer.jsx
+++ src/components/ExpContainer.jsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import Experience from '../data/experience.json'
+
+function ExpContainer () {
+  // Should take some sort of props from JSON folder? Or that would all be handled in Right container???
+  return (
+    <div>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  )
+  
+}
+
+export default ExpContainer;
