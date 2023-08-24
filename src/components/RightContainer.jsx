0befd15fe85db9b7d1f11bf8e3cc7eb411a9ef2c
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-24 10:11:56 -0400

    imported experience json file into right contianer

--- src/components/RightContainer.jsx
+++ src/components/RightContainer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import experience from '../data/experience.json'
 
 function RightContainer () {
   
   return (
     <div>
       <div>
-        
+
       </div>
       <div></div>
       <div></div>
