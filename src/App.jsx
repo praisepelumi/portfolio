4d1655642569147ab19fc1f71d195e5d3d7fbe43
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-26 16:43:27 -0400

    setting up css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import LeftContainer from './components/LeftContainer';
 import RightContainer from './components/RightContainer';
+import styles from '../src/App.module.scss';
 
 function App () {
   return(
-    <div>
-      <LeftContainer />
-      <RightContainer/>
+    <div className={styles.container}>
+      <LeftContainer className={styles.leftSide} />
+      <RightContainer className={styles.rightSide}/>
     </div>
   )
 }
