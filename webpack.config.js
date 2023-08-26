6e459c63d054787445aba619ab101034f776b876
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-08-26 15:35:21 -0400

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,15 @@ module.exports = {
         exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      
     ]
   },
 
